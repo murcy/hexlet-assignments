089120cb181967eea911c779c03230a5f466b66d
Vishnyakov Dmitry <dmitry.vishnyakov@raiffeisen.ru>
2024-05-22 17:42:03 +0300

    first exercise

--- java-spring-ru/first-start/src/main/java/exercise/Application.java
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -6,5 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @GetMapping("/about")
+    String home() {
+        return "Welcome to Hexlet!";
+    }
+}
 // END
