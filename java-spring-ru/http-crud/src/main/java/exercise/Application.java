c2700f3e77d289fbaa7cc3f7973691b42bac3718
Vishnyakov Dmitry <dmitry.vishnyakov@raiffeisen.ru>
2024-05-23 19:11:40 +0300

    solution

---
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -0,0 +1,64 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts")
+    public List<Post> getPosts(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream()
+                .skip((long) page * limit - limit)
+                .limit(limit)
+                .toList();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> getPost(@PathVariable String id) {
+        return posts.stream()
+                .filter(post -> post.getId().equals(id))
+                .findFirst();
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post update(@PathVariable String id, @RequestBody Post post) {
+        var findPost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+
+        if (findPost.isPresent()) {
+            var newPost = findPost.get();
+            newPost.setId(post.getId());
+            newPost.setTitle(post.getTitle());
+            newPost.setBody(post.getBody());
+        }
+        return post;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void delete(@PathVariable String id) {
+        posts.removeIf(post -> post.getId().equals(id));
+    }
+    // END
+}
